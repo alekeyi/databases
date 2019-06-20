75534068c2f8f8dcb06f75aadab1c68e4127f063
alekeyi <aleksey.ioseliani@gmail.com>
2019-06-20 12:28:14 -0500

    Create db tables

--- server/schema.sql
+++ server/schema.sql
@@ -1,15 +1,23 @@
+DROP DATABASE IF EXISTS chat;
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
+CREATE TABLE IF NOT EXISTS users (
   /* Describe your table here.*/
+    user_id INT AUTO_INCREMENT,
+    username VARCHAR(40) NOT NULL,
+    PRIMARY KEY (user_id)
 );
 
-/* Create other tables and define schemas for them here! */
-
-
-
+-- /* Create other tables and define schemas for them here! */
+CREATE TABLE IF NOT EXISTS messages (
+    message_id INT AUTO_INCREMENT,
+    message VARCHAR(40),
+    user_id INT NOT NULL,
+    PRIMARY KEY (message_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
