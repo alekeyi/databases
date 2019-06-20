2ade702773676f3a53175e763ca502b9b1025e87
alekeyi <aleksey.ioseliani@gmail.com>
2019-06-20 17:08:55 -0500

    Return queries for messages and persist

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,17 +2,39 @@ var models = require('../models');
 
 module.exports = {
   messages: {
-    get: function (req, res) {
-      res.status(200);
-      res.send(models.messages.get);
+    get: function(req, res) {
+      console.log('messages.get');
+      models.messages.get((err, data) => {
+        if (err) {
+          res.status(404);
+          res.json(err);
+        } else {
+          console.log('anything');
+          res.status(200);
+
+          // res.send(JSON.stringify(data));
+          res.end();
+        }
+      }, res);
+      // res.status(200);
+      // res.json(models.messages.get);
     }, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    post: function(req, res) {
+      res.status(201);
+      console.log('Post messages!');
+      res.send(models.messages.post);
+    } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
+    get: function(req, res) {
+      res.status(200);
+      res.send(models.messages.user);
+    },
+    post: function(req, res) {
+      res.status(201);
+      res.send(models.messages.user);
+    }
   }
 };
-
--- server/models/index.js
+++ server/models/index.js
@@ -1,15 +1,34 @@
 var db = require('../db');
-
+// console.log(db);
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function(cb, res) {
+      console.log('this is working maybe');
+      db.query('SELECT * FROM messages', (err, rows) => {
+        if (err) {
+          cb(err);
+        } else {
+          // console.log(res);
+          res.send(JSON.stringify(rows));
+        }
+
+        console.log(`success! these are our rows: \n${rows}`);
+      });
+    }, // a function which produces all the messages
+    post: function() {
+      db.query('SELECT * FROM messages', (err, rows) => {
+        if (err) {
+          throw err;
+        }
+
+        console.log(`success! these are our rows: \n${rows}`);
+      });
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function() {},
+    post: function() {}
   }
 };
-
